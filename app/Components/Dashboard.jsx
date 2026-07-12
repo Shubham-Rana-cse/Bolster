081206efe6f8e0f99d6857f02a683b0376751f6d
Shubham-Rana-cse <shubhamrana181945@gmail.com>
2026-07-12 21:44:03 +0530

    fixed next public base url env var

--- app/Components/Dashboard.jsx
+++ app/Components/Dashboard.jsx
@@ -51,7 +51,7 @@ const Dashboard = ({username}) => {
             "description": "Test Transaction",
             "image": "https://example.com/your_logo",
             "order_id": orderId,
-            "callback_url": `${process.env.NEXTAUTH_URL}/api/razorpay`,
+            "callback_url": `${process.env.NEXT_PUBLIC_BASE_URL}/api/razorpay`,
             "prefill": {
                 "name": "Shubham Rana",
                 "email": "shubhamrana181945@gmai.com",
